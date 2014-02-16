41c2ef278c5636725519bc7e620df16f3dead0c9
Marios Kogias <marioskogias@gmail.com>
2014-02-16 23:39:14 +0200

    Start the task_object implementation

---
+++ MatrixMultiplication/tbbs/recursive/task_object.cpp
@@ -0,0 +1,93 @@
+/*
+ * mm_recursive.c
+ *
+ * Routines to realize the recursive matrix multiplication.
+ * RecMult multiplies n by n matrices a and b, putting the result in c, and
+ * using the matrix d as scratch space.  The recursive algorithm is: 
+ *
+ *      d11 = a11 * b11
+ *      c11 = a12 * b21
+ *      d12 = a11 * b12
+ *      c12 = a12 * b22
+ *      d21 = a21 * b11
+ *      c21 = a22 * b21
+ *      d22 = a21 * b12
+ *      c22 = a22 * b22
+ *      c11 = c11 + d11
+ *      c12 = c12 + d12
+ *      c21 = c21 + d21
+ *      c22 = c22 + d22
+ *
+ * where the double indices refer to submatrices in an obvious way.
+ * Each line of RecMult() that recursively calls itself computes one
+ * of the submatrices.  Four scratch half-size matrices are required by the
+ * sequence of computations here.
+ *
+ * The small matrix computations (i.e., for n <= block) can be
+ * optimized considerably from those given here; in particular, this
+ * is important to do before the value of block is chosen optimally. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include <tbb/task.h>
+#include <tbb/task_group.h>
+#include <tbb/task_scheduler_init.h>
+#include <tbb/tick_count.h>
+#include "util.h"
+
+
+int block;
+
+
+class MulTask: public tbb::task {
+
+};
+
+class AddTask: public tbb::task {
+
+};
+
+int main(int argc, char **argv) {
+
+    struct timeval ts,tf;
+    double tt;
+    int n;
+    matrix a, b, c;
+
+    check(argc >= 3, "main: Need matrix size and block size on command line");
+    n = atoi(argv[1]);
+    block=atoi(argv[2]);
+
+    a = newmatrix(n);
+    b = newmatrix(n);
+    c = newmatrix(n);
+    randomfill(n, a);
+    randomfill(n, b);
+
+    gettimeofday(&ts,NULL);
+
+    /*
+     * Implement HERE
+     */
+    
+    gettimeofday(&tf,NULL);
+    tt=(tf.tv_sec-ts.tv_sec)+(tf.tv_usec-ts.tv_usec)*0.000001;
+
+    printf("Recursive Size %d Block %d Time %lf\n",n,block,tt);
+
+    char *filename=(char *)malloc(30*sizeof(char));
+    sprintf(filename,"res_mm_recursive_%d",n);
+    FILE * f=fopen(filename,"w");
+    print(n,c,f);
+    fclose(f);
+
+    freematrix(a,n);
+    freematrix(b,n);
+    freematrix(c,n);
+
+    return 0;
+}
